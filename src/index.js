c2df76f108d8e544554a085d915b6d398bb0df92
Masha-Mascot-00 <mashamascot969@gmail.com>
2020-09-29 23:35:23 +0300

    feat: basic structure to functions min() and max()

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
+exports.min = function min(array) {
+    if (typeof array !== "undefined" && array.length > 0) {
+        let arrayElement, minElement, arrayElementPrevious;
+        for (arrayElement of array) {
+            minElement = minElement === undefined ? arrayElement : minElement;
+            if (arrayElement <= arrayElementPrevious) {
+                minElement =
+                    arrayElement <= minElement ? arrayElement : minElement;
+            }
+            arrayElementPrevious = arrayElement;
+        }
+        return minElement;
+    } else {
+        return 0;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (typeof array !== "undefined" && array.length > 0) {
+        let arrayElement, maxElement, arrayElementPrevious;
+        for (arrayElement of array) {
+            maxElement = maxElement === undefined ? arrayElement : maxElement;
+            if (arrayElement >= arrayElementPrevious) {
+                maxElement =
+                    arrayElement >= maxElement ? arrayElement : maxElement;
+            }
+            arrayElementPrevious = arrayElement;
+        }
+        return maxElement;
+    } else {
+        return 0;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    return 0;
+};
