4578ab2603c653381dfb9f748ec86ce4435b1eee
Masha-Mascot-00 <mashamascot969@gmail.com>
2020-09-30 00:14:14 +0300

    feat: implement avg() function

--- src/index.js
+++ src/index.js
@@ -33,5 +33,20 @@ exports.max = function max(array) {
 };
 
 exports.avg = function avg(array) {
-    return 0;
+    if (typeof array !== "undefined" && array.length > 0) {
+        let numberOfElements = array.length;
+        let sum, arrayElement, averageValue;
+
+        for (arrayElement of array) {
+            if (isNaN(sum)) {
+                sum = arrayElement;
+            } else {
+                sum += arrayElement;
+                averageValue = sum / numberOfElements;
+            }
+        }
+        return averageValue;
+    } else {
+        return 0;
+    }
 };
